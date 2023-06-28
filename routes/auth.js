daeeaf01005dbf9d3158004069507d8eede30545
Jonatas Borges <rufinoelaine61@gmail.com>
2023-06-28 11:59:04 -0300

    all

--- routes/auth.js
+++ routes/auth.js
@@ -1,36 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const pool = require("../config/db");
 
+router.get("/login", (req, res) => {
+  res.render("login", { layout: "layouts/login-layout" });
+});
 
+router.post("/login", async (req, res, next) => {
+  console.log("oii");
+  try {
+    const {logemail, logpass } = req.body;
+    const sql = `SELECT * FROM user WHERE email =?`;
+    const values = [logemail];
+    const [rows] = await pool.query(sql, values);
 
-router.get('/login', (req, res) => {
-    res.render('login', { layout: 'layouts/login-layout' });
-  });
+    if (rows.length === 0) {
+      return res.redirect("/login");
+    }
+    if (logpass === rows[0].password) {
+      req.session.user = rows[0];
+      return res.redirect("/");
+    }
 
-  
-  router.post('/login', async(req, res, next) => {
-    console.log('oii');
+    return res.redirect("/login");
+  } catch (err) {
+    console.error(err);
+    next(new Error("Ocorreu um erro inesperado."));
+  }
+});
+
+router.post("/cadastrar", async (req, res, next) => {
+  try {
+    const { logname, logemail, logpass } = req.body;
+    const sql = `insert into user (name, email, password) values (?, ?,?)`;
+    const values = [logname, logemail, logpass];
+    const [rows] = await pool.query(sql, values);
+
     
-    try {
-        const {logemail, logpass} = req.body;
-        const sql = `SELECT * FROM user WHERE email =?`;
-        const values = [logemail];
-        const [rows] = await pool.query(sql, values);
 
-        if (rows.length === 0) {
-            return res.redirect("/login");
-        }
-        if(logpass === rows[0].password){
-            req.session.user = rows[0];
-            return res.redirect("/");
-        }
-
-        return res.redirect("/login");
-      } catch (err) {
-        console.error(err);
-        next(new Error("Ocorreu um erro inesperado."));
-      }
-  });
+    return res.redirect("/");
+  } catch (err) {
+    console.error(err);
+    next(new Error("Ocorreu um erro inesperado."));
+  }
+});
 
 module.exports = router;
--- routes/descricao.js
+++ routes/descricao.js
@@ -1,11 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const pool = require("../config/db");
 
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const sql = "SELECT * FROM product WHERE id = ?";
+    const values = [id];
+    const [rows] = await pool.query(sql, values);
 
-
-router.get('/', (req, res) => {
-    res.render('products/descricao', { layout: 'layouts/base' });
-  });
-  
+    if (rows.length === 0) {
+      next(createError(404));
+    }
+    res.render("products/descricao", { layout: "layouts/base", product: rows[0] });
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 module.exports = router;
